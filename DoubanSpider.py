084a097d2b6fabf916bac6292f481e2bbee18bbb
liuhaohust <liuhao02@baidu.com>
2016-10-18 21:41:14 +0800

    add DoubanSpider.py

---
+++ DoubanSpider.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+import sys
+
+class DoubanSpider(scrapy.Spider):
+    # 必须定义
+    name = "douban"
+    # 初始urls
+    start_urls = ["https://book.douban.com/subject/1676611/",
+            "https://read.douban.com/ebook/24791669/"]
+
+    # 默认response处理函数
+    def parse(self, response):
+        # 把结果写到文件中
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
